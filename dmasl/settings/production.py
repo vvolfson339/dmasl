2a1f5b2f4b4c02b2774119ac5c37c5e6302f9829
Victor Volfson <57919754+vvolfson339@users.noreply.github.com>
2020-12-24 17:43:03 -0500

    update settings

--- dmasl/settings/production.py
+++ dmasl/settings/production.py
@@ -8,14 +8,14 @@ DEBUG = True
 
 SECRET_KEY = config['SECRET_KEY']
 
-ALLOWED_HOSTS = config['Allowed_Hosts']
+ALLOWED_HOSTS = ['159.89.146.14', 'dmasl.ca', 'www.dmasl.ca']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': config['MySQL_DB_Name'],
         'USER': config['MySQL_DB_User'],
-        'PASSWORD': config['MySQL_DB_Password'],
+        'PASSWORD': config['MySQL_DB_Pass'],
         'HOST': config['MySQL_DB_Host'],
     }
 }
